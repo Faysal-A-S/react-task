a32c4accc4759e13d6d30b36b8231a991fd33213
Faysal Ahmed <hl.faysal1@gmail.com>
2023-09-29 18:13:23 +0600

    completed problem 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import Problem1 from "./components/Problem-1.jsx";
 import Menu from "./components/Menu.jsx";
 import Problem2 from "./components/Problem-2.jsx";
 import Index from "./components/Index.jsx";
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/" element={<Menu />}>
-          <Route path="problem-1" element={<Problem1 />} />
-          <Route path="problem-2" element={<Problem2 />} />
-        </Route>
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Menu />}>
+            <Route path="problem-1" element={<Problem1 />} />
+            <Route path="problem-2" element={<Problem2 />} />
+          </Route>
+        </Routes>
+      </Provider>
     </>
   );
 }
--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,26 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Problem1 = () => {
   const [show, setShow] = useState("all");
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("");
+  const list = [];
+  let [lists, setLists] = useState(list);
 
   const handleClick = (val) => {
     setShow(val);
   };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
 
+    if (name !== "" && status !== "") {
+      setLists((oldData) => [...oldData, { name, status }]);
+
+      setLists(newList);
+    }
+
+    setName("");
+    setStatus("");
+  };
   return (
     <div className="container">
       <div className="row justify-content-center mt-5">
         <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
         <div className="col-6 ">
-          <form className="row gy-2 gx-3 align-items-center mb-4">
+          <form
+            className="row gy-2 gx-3 align-items-center mb-4"
+            onSubmit={handleOnSubmit}
+          >
             <div className="col-auto">
-              <input type="text" className="form-control" placeholder="Name" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div className="col-auto">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Status"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
               />
             </div>
             <div className="col-auto">
@@ -68,7 +94,24 @@ const Problem1 = () => {
                 <th scope="col">Status</th>
               </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+              {lists
+
+                .filter((data) => {
+                  if (data.status === show) {
+                    return data.status;
+                  } else if (show === "all") {
+                    return data.status;
+                  }
+                })
+
+                .map((data) => (
+                  <tr>
+                    <td>{data.name}</td>
+                    <td>{data.status}</td>
+                  </tr>
+                ))}
+            </tbody>
           </table>
         </div>
       </div>
